2ca29573df131636fee95a685a8bece9d401e4be
Gautham P R <gauthampr18@gmail.com>
2020-11-09 10:55:56 +0530

    Minor

--- Exp_4/e.sql
+++ Exp_4/e.sql
@@ -7,15 +7,14 @@ BEGIN
     FOR p_employee IN (SELECT * from employee)
     LOOP
         IF (p_employee.salary < 10000) THEN
-            dbms_output.put_line('this');
             UPDATE employee
                 SET salary = salary + 1000
                 WHERE id=p_employee.id;
             p_count := p_count+1;
-            dbms_output.put_line(p_employee.id || ' ' || p_employee.salary);
         END IF;
     END LOOP;
-    dbms_output.put_line(p_count);
+    dbms_output.put_line('Number of Updated Users:' || p_count);
 END;
 /
 SELECT * FROM employee;
+rollback;

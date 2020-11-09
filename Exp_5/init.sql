249ba350fa8ec5d23950d5666af25104fe1bc898
Gautham P R <gauthampr18@gmail.com>
2020-11-09 11:40:37 +0530

    minor mods

--- Exp_5/init.sql
+++ Exp_5/init.sql
@@ -1,6 +1,6 @@
-CREATE TABLE employee(id INT, salary float);
+CREATE TABLE employee(id INT UNIQUE, salary float);
 
 INSERT INTO employee(id, salary) VALUES(1234, 50000);
 INSERT INTO employee(id, salary) VALUES(4321, 20000);
 INSERT INTO employee(id, salary) VALUES(2345, 1800);
-INSERT INTO employee(id, salary) VALUES(1234, 6000);
+INSERT INTO employee(id, salary) VALUES(5432, 6000);

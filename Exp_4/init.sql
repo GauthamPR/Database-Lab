15d98cb5432aa4a7d0af8030053cc52b84b94b6d
Gautham P R <gauthampr18@gmail.com>
2020-11-09 11:19:34 +0530

    Added init files

---
+++ Exp_4/init.sql
@@ -0,0 +1,6 @@
+CREATE TABLE employee(id INT, salary float);
+
+INSERT INTO employee(id, salary) VALUES(1234, 50000);
+INSERT INTO employee(id, salary) VALUES(4321, 20000);
+INSERT INTO employee(id, salary) VALUES(2345, 1800);
+INSERT INTO employee(id, salary) VALUES(1234, 6000);

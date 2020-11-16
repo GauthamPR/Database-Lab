00e99b9d3114578a0e38026ad28555b07587575f
Gautham P R <gauthampr18@gmail.com>
2020-11-16 17:21:03 +0530

    Minor

--- Exp_5/b.sql
+++ Exp_5/b.sql
@@ -3,6 +3,7 @@ set serveroutput on;
 
 SELECT * FROM employee;
 
+CREATE OR REPLACE PROCEDURE incr() IS
 DECLARE
     p_id employee.id%type;
     p_salary employee.salary%type;
@@ -11,13 +12,7 @@ BEGIN
     UPDATE employee
         SET salary = salary + ((salary/100)*6)
         WHERE id=p_id;
-EXCEPTION
-    WHEN no_data_found THEN
-        dbms_output.put_line('No Such Employee');
-    WHEN too_many_rows THEN
-        dbms_output.put_line('Too Many Employees');
 END;
 /
 
 SELECT * FROM employee;
-rollback;

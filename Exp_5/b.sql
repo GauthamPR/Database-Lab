c0c213506b704d169f724e911d4047c38b52d350
Gautham P R <gauthampr18@gmail.com>
2020-11-16 17:25:27 +0530

    Minor

--- Exp_5/b.sql
+++ Exp_5/b.sql
@@ -6,11 +6,15 @@ SELECT * FROM employee;
 DECLARE
     p_id employee.id%type;
     p_salary employee.salary%type;
+PROCEDURE incr IS
 BEGIN
     SELECT id, salary INTO p_id, p_salary FROM employee WHERE salary > 10000;
     UPDATE employee
         SET salary = salary + ((salary/100)*6)
         WHERE id=p_id;
+END;
+BEGIN
+    incr();
 EXCEPTION
     WHEN no_data_found THEN
         dbms_output.put_line('No Such Employee');
